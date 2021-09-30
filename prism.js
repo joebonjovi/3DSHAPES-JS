f97fea9437045976cc5d1448afedb5110da6c804
joebonjovi <64037041+joebonjovi@users.noreply.github.com>
2021-09-30 14:03:36 -0400

    Add files via upload

---
+++ prism.js
@@ -0,0 +1,17 @@
+
+function drawPrism(offx, offy, l, w, h, r){
+var c = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");
+ctx.beginPath();
+ctx.clearRect(0, 0, c.width, c.height);
+for(i = 0; i < l; i++){
+if(i<l-2){
+ctx.fillStyle = "darkgrey";
+}
+ctx.fillRect(i*r+offx, i+offy, w, h);
+if(i == l-2){
+ctx.fillStyle = "lightgrey";
+}
+}
+ctx.stroke();
+}
